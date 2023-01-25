f1c33fdc2c366bb27f34ed14594b6a8ba273070c
Nsubuga Raymond <32416487+nsubugaraymond@users.noreply.github.com>
2023-01-25 17:10:58 +0300

    User Api Update

--- Api/User_Api.js
+++ Api/User_Api.js
@@ -1,12 +1,18 @@
 import ApiManager from "./ApiManager";
 
 export const user_login = async data => {
-  
     try {
-      
+      const result = await ApiManager("/login", {
+          method:"POST",
+          headers:{
+             'Content-Type':"application/json"
+
+          },
+          data:data,
+      });
+      return result;
   } catch (error) {
-      
+    return error.response.data 
   }
 
-
-}
+};
